373d2ce870db3fe4326066ae84d36de88a23dece
Okami <dyfault-crypto@pm.me>
2023-06-13 23:20:38 +0200

    add content to self hosted server

--- src/page/projects/debian/DebianSelfHosted.js
+++ src/page/projects/debian/DebianSelfHosted.js
@@ -1,9 +1,40 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Center, Divider, Stack } from "@chakra-ui/react";
+import { ScrollToTopButton } from "../../../components/button/ScrollToTopButton";
+import { NavButton } from "../../../components/menu/NavButton";
 
 export const DebianSelfHosted = () => {
+
+    const content = 
+        <Box w={['375px', null, '650px', '750px', '1000px', '1250px']}>
+
+            <Stack>
+                <Center>
+                    <Text fontWeight='bold' fontSize='xl'>Self Hosted Server</Text> 
+                </Center> 
+                <Divider />
+            </Stack>
+
+            <Center mt='16px'>
+                <Text>Project presentation: </Text> 
+            </Center>
+
+            <Text mt='16px' textAlign='left'>
+                This server is a Raspberry Pi 4 running Raspbian. I utilize it to host a personal cloud server that employs an Apache web server. 
+                Additionally, my Raspberry Pi serves as the hosting platform for all my Discord bots. 
+                To generate the SSL certificate for my cloud server, I use Certbot. For running my Discord bots, 
+                I utilize bash scripts that launch them using the 'screen' command, and in case of errors, automatically restart them.
+            </Text>
+
+            <Center mt='16px'>
+                <NavButton variant='skipnavbutton' to='/js-discord-bot'>Discord Bot presentation</NavButton>
+            </Center>
+        </Box>
+
     return (
         <Box>
-            Debian slef hosted
+            {content}
+
+            <ScrollToTopButton />
         </Box>
     )
 }
--- src/page/projects/debian/DebianVps.js
+++ src/page/projects/debian/DebianVps.js
@@ -1,4 +1,4 @@
-import { Box, Text, Center, Divider, Stack, Button } from "@chakra-ui/react";
+import { Box, Text, Center, Divider, Stack } from "@chakra-ui/react";
 import { ScrollToTopButton } from "../../../components/button/ScrollToTopButton";
 import { NavButton } from "../../../components/menu/NavButton";
 
@@ -9,7 +9,7 @@ export const DebianVps = () => {
 
             <Stack>
                 <Center>
-                    <Text fontWeight='bold' fontSize='xl'> Web3 App </Text> 
+                    <Text fontWeight='bold' fontSize='xl'> VPS Server</Text> 
                 </Center> 
                 <Divider />
             </Stack>
@@ -18,7 +18,7 @@ export const DebianVps = () => {
                 <Text>Project presentation: </Text> 
             </Center>
 
-            <Text textAlign='left'>
+            <Text mt='16px' textAlign='left'>
                 This server is a VPS that I rent. Initially, I chose it to host my web3 application and this portfolio. 
                 This has allowed me to learn how to set up a reverse proxy to host multiple websites with a single server. 
                 To run my two sites, I use pm2, and it's my first time using nginx as a web server. Finally, the wildcard SSL certificate was generated using certbot.
