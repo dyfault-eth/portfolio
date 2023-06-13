94b381897e10b80ec83e82bc730b399a7e97d0df
Okami <dyfault-crypto@pm.me>
2023-06-13 22:08:22 +0200

    add vps content

--- src/page/projects/debian/DebianVps.js
+++ src/page/projects/debian/DebianVps.js
@@ -1,9 +1,39 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Center, Divider, Stack, Button } from "@chakra-ui/react";
+import { ScrollToTopButton } from "../../../components/button/ScrollToTopButton";
+import { NavButton } from "../../../components/menu/NavButton";
 
 export const DebianVps = () => {
+
+    const content = 
+        <Box w={['375px', null, '650px', '750px', '1000px', '1250px']}>
+
+            <Stack>
+                <Center>
+                    <Text fontWeight='bold' fontSize='xl'> Web3 App </Text> 
+                </Center> 
+                <Divider />
+            </Stack>
+
+            <Center mt='16px'>
+                <Text>Project presentation: </Text> 
+            </Center>
+
+            <Text textAlign='left'>
+                This server is a VPS that I rent. Initially, I chose it to host my web3 application and this portfolio. 
+                This has allowed me to learn how to set up a reverse proxy to host multiple websites with a single server. 
+                To run my two sites, I use pm2, and it's my first time using nginx as a web server. Finally, the wildcard SSL certificate was generated using certbot.
+            </Text>
+
+            <Center mt='16px'>
+                <NavButton variant='skipnavbutton' to='/js-web3-app'>Web3 App presentation</NavButton>
+            </Center>
+        </Box>
+
     return (
         <Box>
-            Debian vps
+            {content}
+
+            <ScrollToTopButton />
         </Box>
     )
 }
--- src/page/projects/js/Web3App.js
+++ src/page/projects/js/Web3App.js
@@ -25,159 +25,152 @@ export const Web3App = () => {
     };
 
     const content = 
-    <Box w={['375px', null, '650px', '750px', '1000px', '1250px']}>
+        <Box w={['375px', null, '650px', '750px', '1000px', '1250px']}>
 
-    <Stack>
-        <Center>
-            <Text fontWeight='bold' fontSize='xl'>Web3 App</Text>
-        </Center>
-        <Divider />
-    </Stack>
+            <Stack>
+                <Center>
+                    <Text fontWeight='bold' fontSize='xl'> Web3 App </Text> 
+                </Center> 
+                <Divider />
+            </Stack>
 
-    <Stack>
-        <Center mt='12px' display="flex" alignItems='center'>
-            You can check the project
-            
-            <Button variant="urlbutton" marginLeft="-0.6rem" as="a" href="https://app.dyfault.com" target="_blank" rel="noreferrer">here</Button>
-        </Center>
-        
-        <Center>
-            <Divider w='300px' />
-        </Center>
+            <Stack>
+                <Center mt='12px' display="flex" alignItems='center'>
+                    You can check the project
 
-    </Stack>
+                <Button variant="urlbutton" marginLeft="-0.6rem" as="a" href="https://app.dyfault.com" target="_blank" rel="noreferrer">here</Button> 
+                </Center>
 
-    <Stack mt='16px'>
-        <Center>
-            <Text>Project presentation : </Text>
-        </Center>
-
-        <Text>
-            This is my first web project. I created it with the aim of learning React.js and gaining the ability to build a website. 
-            The purpose of this project is to create a community web application for one or multiple projects in cryptocurrency, 
-            which means that this site directly interacts with the Polygon blockchain. In this project, I also use Redux Toolkit, 
-            which I have learned to use, as well as Chakra UI and other libraries related to Web3, 
-            particularly WalletConnect with Web3Modal to connect any type of wallet. This project is intended to evolve over time.
-        </Text>
-
-    </Stack>
-
-    <Stack>
-        <Text mt='16px'>
-            Due to Coingecko's increasing limitations on free API requests for cryptocurrency prices and graphs, 
-            I have implemented three Node.js scripts that make API calls at specific intervals. 
-            These scripts retrieve the data and write the responses to JSON files located at the root of my website. 
-            In the frontend, I have a function called within my Redux implementation that retrieves the data from these JSON files. 
-            This approach allows me to work around the API limitations and ensure consistent access to the cryptocurrency data for the homepage.
-        </Text>
-
-        <Center>
-            <SimpleGrid columns={{base: 1, lg: 2}} mt='24px' gap={2}>
-                <Button as={SkipNavLink} id='btchist' href='btchist' variant='skipnavbutton'>Skip to Fear History code</Button>
-
-                <Button as={SkipNavLink} id='fearhist' href='fearhist' variant='skipnavbutton'>Skip to Fear History code</Button>
-            </SimpleGrid>
-        </Center>
-        
-        
-
-        <Box position='relative'>
-            <Center mt='24px'>
-                Example of crypto-prices Bash script :
+            <Center>
+                <Divider w='300px' />
             </Center>
 
-            <SyntaxHighlighter language="bash" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}>
-                {cryptoPriceBash}
-            </SyntaxHighlighter>
-            <IconButton onClick={() => handleCopyClick(cryptoPriceBash)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} />
-        </Box>
+            </Stack>
 
-        <Box position='relative'>
-            <Center mt='24px'>
-                Example of crypto-prices Node.js script :
+            <Stack mt='16px'>
+                <Center>
+                    <Text> Project presentation: </Text> 
+                </Center>
+
+                <Text>
+                This is my first web project.I created it with the aim of learning React.js and gaining the ability to build a website.
+                The purpose of this project is to create a community web application
+                for one or multiple projects in cryptocurrency,
+                which means that this site directly interacts with the Polygon blockchain.In this project, I also use Redux Toolkit,
+                which I have learned to use, as well as Chakra UI and other libraries related to Web3,
+                particularly WalletConnect with Web3Modal to connect any type of wallet.This project is intended to evolve over time. 
+                </Text>
+
+            </Stack>
+
+            <Stack>
+                <Text mt='16px'>
+                    Due to Coingecko 's increasing limitations on free API requests for cryptocurrency prices and graphs, 
+                    I have implemented three Node.js scripts that make API calls at specific intervals.
+                    These scripts retrieve the data and write the responses to JSON files located at the root of my website.
+                    In the frontend, I have a
+                    function called within my Redux implementation that retrieves the data from these JSON files.
+                    This approach allows me to work around the API limitations and ensure consistent access to the cryptocurrency data
+                    for the homepage. 
+                </Text>
+
+                <Center>
+                    <SimpleGrid columns={{ base: 1, lg: 2 }} mt='24px' gap={ 2 } >
+                    <Button as={SkipNavLink} id='btchist' href='btchist' variant='skipnavbutton'>Skip to Bitcoin History code</Button>
+
+                    <Button as={SkipNavLink} id='fearhist' href='fearhist' variant='skipnavbutton'>Skip to Fear History code</Button> </SimpleGrid> 
+                </Center>
+
+
+
+            <Box position='relative'>
+                <Center mt='24px' >
+                    Example of crypto - prices Bash script:
+                </Center>
+
+            <SyntaxHighlighter language="bash" style={ colorMode === 'dark' ? atomDark : solarizedlight } customStyle={{ fontSize: '14px'}}>{cryptoPriceBash}</SyntaxHighlighter> 
+            <IconButton onClick = {() => handleCopyClick(cryptoPriceBash)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />}/> 
+            </Box>
+
+            <Box position='relative'>
+                <Center mt='24px'>
+                    Example of crypto - prices Node.js script:
+                </Center>
+
+                <SyntaxHighlighter language="javascript" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{ fontSize: '14px'}}>{cryptoPriceJS}</SyntaxHighlighter> 
+                <IconButton onClick = {() => handleCopyClick(cryptoPriceJS) } position="absolute" top="63px" right="8px" size="sm" icon={< MdContentCopy />}/> 
+            </Box>
+
+            <Box position='relative'>
+                <Center mt='24px'>
+                    Example of the fetchPrice frontend function :
+                </Center>
+
+                <SyntaxHighlighter language="javascript" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{ fontSize: '14px' } }>
+                    {cryptoPriceFront}
+                </SyntaxHighlighter> 
+                <IconButton onClick = {() => handleCopyClick(cryptoPriceFront)} position="absolute" top="63px" right="8px" size="sm" icon={< MdContentCopy />}/> 
+            </Box>
+
+            <SkipNavContent id = 'btchist'/>
+
+                <Center>
+                    <Divider w={['350px', null, '650px', '750px', '850px', '950px']} mt='24px' / >
+                </Center>
+
+            <Box position='relative' >
+                <Center mt='24px' >
+                    Example of Bitcoin history Bash script:
+                </Center>
+
+                <SyntaxHighlighter language="bash" style={ colorMode === 'dark' ? atomDark : solarizedlight } customStyle={{ fontSize: '14px' }}>{ btcHistBash }</SyntaxHighlighter> 
+                <IconButton onClick = {() => handleCopyClick(btcHistBash) } position = "absolute" top = "63px" right = "8px" size = "sm" icon = {< MdContentCopy />}/> 
+            </Box>
+
+            <Box position='relative'>
+                <Center mt='24px' >
+                    Example of Bitcoin history Node.js script:
+                </Center>
+
+                <SyntaxHighlighter language="javascript" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle = {{ fontSize: '14px' }}>{btcHistJS}</SyntaxHighlighter> 
+                <IconButton onClick = {() => handleCopyClick(btcHistJS) } position = "absolute" top = "63px" right = "8px" size = "sm" icon = {<MdContentCopy />}/> 
+            </Box>
+
+            <SkipNavContent id='fearhist' />
+
+            <Center>
+                <Divider w={['350px', null, '650px', '750px', '850px', '950px']} mt = '24px' / >
             </Center>
 
-            <SyntaxHighlighter language="javascript" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}>
-                {cryptoPriceJS}
-            </SyntaxHighlighter>
-            <IconButton onClick={() => handleCopyClick(cryptoPriceJS)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} />
+            <Box position = 'relative'>
+                <Center mt = '24px'>
+                    Example of Fear and Greed history Bash script:
+                </Center>
+
+                <SyntaxHighlighter language="bash" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}> 
+                    {fearHistBash} 
+                </SyntaxHighlighter> 
+                <IconButton onClick={() => handleCopyClick(fearHistBash)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} /> 
+            </Box>
+
+                <Box position = 'relative' >
+                    <Center mt = '24px'>
+                        Example of Fear and Greed history Node.js script:
+                    </Center>
+
+                    <SyntaxHighlighter language = "javascript" style = {colorMode === 'dark' ? atomDark : solarizedlight} customStyle = {{fontSize: '14px'}}> 
+                        {fearHistJS} 
+                    </SyntaxHighlighter> 
+                    <IconButton onClick={() => handleCopyClick(fearHistJS)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} /> 
+                </Box>
+            </Stack>
         </Box>
 
-        <Box position='relative'>
-            <Center mt='24px'>
-                Example of the fetchPrice frontend function :
-            </Center>
-
-            <SyntaxHighlighter language="javascript" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}>
-                {cryptoPriceFront}
-            </SyntaxHighlighter>
-            <IconButton onClick={() => handleCopyClick(cryptoPriceFront)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} />
-        </Box>
-
-        <SkipNavContent id='btchist' />
-
-        <Center>
-            <Divider w={['350px', null, '650px', '750px', '850px', '950px']} mt='24px' />
-        </Center>
-        
-        <Box position='relative'>
-            <Center mt='24px'>
-                Example of Bitcoin history Bash script :
-            </Center>
-
-            <SyntaxHighlighter language="bash" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}>
-                {btcHistBash}
-            </SyntaxHighlighter>
-            <IconButton onClick={() => handleCopyClick(btcHistBash)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} />
-        </Box>
-
-        <Box position='relative'>
-            <Center mt='24px'>
-                Example of Bitcoin history Node.js script :
-            </Center>
-
-            <SyntaxHighlighter language="javascript" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}>
-                {btcHistJS}
-            </SyntaxHighlighter>
-            <IconButton onClick={() => handleCopyClick(btcHistJS)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} />
-        </Box>
-
-        <SkipNavContent id='fearhist' />
-
-        <Center>
-            <Divider w={['350px', null, '650px', '750px', '850px', '950px']} mt='24px' />
-        </Center>
-
-        <Box position='relative'>
-            <Center mt='24px'>
-                Example of Fear and Greed history Bash script :
-            </Center>
-
-            <SyntaxHighlighter language="bash" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}>
-                {fearHistBash}
-            </SyntaxHighlighter>
-            <IconButton onClick={() => handleCopyClick(fearHistBash)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} />
-        </Box>
-
-        <Box position='relative'>
-            <Center mt='24px'>
-                Example of Fear and Greed history Node.js script :
-            </Center>
-
-            <SyntaxHighlighter language="javascript" style={colorMode === 'dark' ? atomDark : solarizedlight} customStyle={{fontSize: '14px'}}>
-                {fearHistJS}
-            </SyntaxHighlighter>
-            <IconButton onClick={() => handleCopyClick(fearHistJS)} position="absolute" top="63px" right="8px" size="sm" icon={<MdContentCopy />} />
-        </Box>
-
-    </Stack>
-</Box>
-
-    return (
-        <Center>
+    return( 
+        <Center> 
             {content}
-
             <ScrollToTopButton />
         </Center>
     )
+    
 }
